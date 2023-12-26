02b88308f7c7682d45e93c80a6a5132c892848fa
GeorgStrassberger <georg.strassberger@googlemail.com>
2023-12-26 10:28:12 +0100

    adding inc-files & display all firtstLetters

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
     <title>Geburtsstatistiken</title>
 </head>
 <body>
-    
+    <?php 
+    require_once("inc/names.php");
+
+    // Asoziatives Array
+    $firstLetters = [];
+
+    foreach($names AS $nameArray){
+        $nameFirstLetter = $nameArray['name'][0];
+
+        // Ist der Buchstabe vorhanden? 
+        if(empty($firstLetters[$nameFirstLetter])){
+            // Setze den wert auf true = JA 
+            $firstLetters[$nameFirstLetter] = true;
+        }
+    }
+    var_dump($firstLetters);
+    ?>
+
+    <p>
+        <?php foreach($firstLetters AS $firstLetter => $bool): ?>
+        <a href="index.php?car=<?php echo $firstLetter ?>"><?php echo $firstLetter ?></a>
+        <?php endforeach; ?>
+    </p>
 </body>
 </html>
