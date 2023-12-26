ed09d2e45b9eaf49c56ef6794914c5942173f171
GeorgStrassberger <georg.strassberger@googlemail.com>
2023-12-26 10:56:37 +0100

    add css styles

--- index.php
+++ index.php
@@ -4,30 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Geburtsstatistiken</title>
+    <link rel="stylesheet" type="text/css" href="./styles/simple.css">
+    <link rel="stylesheet" type="text/css" href="./styles/custom.css">
 </head>
 <body>
+    <header>
+        <h1>Geburtsstatistiken</h1>
+        <p>Hier sind die Namen aus den Geburtsstatistiken der USA aufgelistet.</p>
+        <p>Die Top: 1000, von Jahr 1980 bis zum Jahr 2014.</p>
+    </header>
+
     <?php 
+    // Datei nur einmal laden
     require_once("inc/names.php");
 
-    // Asoziatives Array
+    //Neues Asoziatives Array
     $firstLetters = [];
 
+    // Datei einträge filtern
     foreach($names AS $nameArray){
+        // Den ersten Buchstaben abgreifen
         $nameFirstLetter = $nameArray['name'][0];
 
         // Ist der Buchstabe vorhanden? 
         if(empty($firstLetters[$nameFirstLetter])){
-            // Setze den wert auf true = JA 
+            // Setze den Wert auf true. 
             $firstLetters[$nameFirstLetter] = true;
         }
     }
     var_dump($firstLetters);
     ?>
 
-    <p>
+    <nav class="nav">
         <?php foreach($firstLetters AS $firstLetter => $bool): ?>
-        <a href="index.php?car=<?php echo $firstLetter ?>"><?php echo $firstLetter ?></a>
+            <a 
+                href="index.php?char=<?php echo $firstLetter ?>"
+                <?php if(!empty($_GET['char']) && $_GET['char'] === $firstLetter): ?>
+                    class="selected"
+                <?php endif; ?>
+            ><?php echo $firstLetter ?></a>
         <?php endforeach; ?>
-    </p>
+    </nav>
+
 </body>
 </html>
